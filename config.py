726f2d97613c05f8fba61269bcc4b40e66ac78c3
Nurbolottop <erk1nbaevw2711@gmail.com>
2023-02-15 05:52:57 -0500

    Other Settings

--- config.py
+++
@@ -1,5 +0,0 @@
-SECRET_KEY = 'django-insecure-7er(%l$-w4aw_1n6k@7-=#86u7j0+mm3v2ibc(ad+9784cp13f'
-DEBUG = True
-
-EMAIL_HOST_USER = "erk1nbaevw2711@gmail.com"
-EMAIL_HOST_PASSWORD = "chwpgztlzveomblr"
